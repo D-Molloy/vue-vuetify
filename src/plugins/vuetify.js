60c9cb8738620ddd6dfe09d47e2687262be74d6c
Denis Molloy <denis.molloy@gmail.com>
2020-02-17 17:57:10 -0800

    setup routes and linked to nav drawer

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,4 +3,15 @@ import Vuetify from 'vuetify/lib';
 import '@mdi/font/css/materialdesignicons.css';
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#9652ff',
+        success: '#3cd1c2',
+        info: '#ffaa2c',
+        error: '#f83e70'
+      }
+    }
+  }
+});
