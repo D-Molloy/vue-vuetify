5538d14005a4c13962068426d22a06e226ce409d
Denis Molloy <denis.molloy@gmail.com>
2020-02-17 17:12:54 -0800

    finished tutorial, started app

---
+++ src/views/BreakPoints.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>Home Page</h1>
+    <v-btn class="hidden-md-and-down">Hidden on MD and down</v-btn>
+    <v-btn color="purple" dark class="hidden-sm-and-up"
+      >Hidden on MD and up</v-btn
+    >
+    <v-btn color="blue" dark class="hidden-sm-only">hidden on small</v-btn>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Home'
+};
+</script>
